f82256fe367d785ac28776933d80cbc142c4dbc2
JP Negri <jp@negri.net.br>
2024-02-14 13:23:45 -0300

    dev: sensor refreshing is working.

--- OpenHardwareMonitorMqtt/MonitorCommand.cs
+++ OpenHardwareMonitorMqtt/MonitorCommand.cs
@@ -3,20 +3,22 @@ using CliFx.Attributes;
 using CliFx.Exceptions;
 using CliFx.Infrastructure;
 using OpenHardwareMonitor.Hardware;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace OpenHardwareMonitor.Mqtt;
 
 [Command(Description = "Monitors this machine hardware and publish values to a MQTT broker.")]
 public class MonitorCommand : ICommand
 {
-    
+
     [CommandParameter(0, Description = "Address or name of a MQTT broker", Name = nameof(Broker))]
     public string Broker { get; set; } = string.Empty;
 
     [CommandOption(nameof(Port), 'p', Description = "Port number the MQTT broker is listening.")]
     public int Port { get; set; } = 1883;
 
-    [CommandOption(nameof(UseTls),  Description = "If TLS should be used.")]
+    [CommandOption(nameof(UseTls), Description = "If TLS should be used.")]
     public bool UseTls { get; set; } = false;
 
     [CommandOption(nameof(ValidateTlsCert), Description = "If TLS certificates should be validated.")]
@@ -26,25 +28,34 @@ public class MonitorCommand : ICommand
     public bool Verbose { get; set; } = false;
 
     [CommandOption(nameof(SensorTypes), 's', Description = "Sensor types to publish.")]
-    public SensorType[] SensorTypes { get; set; } = { SensorType.Temperature, SensorType.Power };
+    public HashSet<SensorType> SensorTypes { get; set; } = new() { SensorType.Temperature, SensorType.Power, SensorType.Load };
 
     [CommandOption(nameof(Components), 'c', Description = "Components to publish.")]
-    public Component[] Components { get; set; } = { Component.MainBoard, Component.Cpu, Component.Gpu };
+    public HashSet<Component> Components { get; set; } = new() { Component.MainBoard, Component.Cpu, Component.Gpu };
 
-    public ValueTask ExecuteAsync(IConsole console)
-    {       
+    [CommandOption(nameof(DoAdminCheck), Description = "Check for admin permissions.")]
+    public bool DoAdminCheck { get; set; } = true;
+
+    [CommandOption(nameof(Pooling), Description = "Keeps the program running and pooling sensor data.")]
+    public bool Pooling { get; set; } = false;
+
+    [CommandOption(nameof(PoolingInterval), Description = "Pooling interval in seconds.")]
+    public int PoolingInterval { get; set; } = 5;
+
+    public async ValueTask ExecuteAsync(IConsole console)
+    {
         if (string.IsNullOrWhiteSpace(Broker))
         {
             throw new CommandException("A broker must be supplied.", 2);
         }
 
-        if (SensorTypes.Length <= 0)
+        if (SensorTypes.Count <= 0)
         {
             throw new CommandException("At least one sensor type must be pooled.", 2);
         }
         var sensors = new HashSet<SensorType>(SensorTypes);
 
-        if (Components.Length <= 0)
+        if (Components.Count <= 0)
         {
             throw new CommandException("At least one component must be pooled.", 2);
         }
@@ -53,13 +64,16 @@ public class MonitorCommand : ICommand
         {
             throw new CommandException("At least one component must be pooled.", 2);
         }
-        
-        if (!IsUserAdministrator())
+
+        if (DoAdminCheck)
         {
-            throw new CommandException("This command requires administrative privileges to execute.", 1);
+            if (!IsUserAdministrator())
+            {
+                throw new CommandException("This command requires administrative privileges to execute.", 1);
+            }
         }
 
-        var output = Verbose ? console.Output : null;
+        var verboseOutput = Verbose ? console.Output : null;
 
         try
         {
@@ -76,28 +90,72 @@ public class MonitorCommand : ICommand
 
             computer.Open();
 
-            var allHardware = computer.Hardware ?? throw new CommandException("No hardware could be read!", 10);
-            foreach(var hardware in allHardware)
+            var visitor = new SensorVisitor(sensor =>
             {
-                output?.WriteLine($"H {hardware.HardwareType}: {hardware.Identifier}: {hardware.Name}");
-                foreach(var sensor in hardware.Sensors.Where(s => sensors.Contains(s.SensorType)))
-                {
-                    output?.WriteLine($"S {sensor.SensorType}: {sensor.Identifier}: {sensor.Name} = {sensor.Value}");
-                }
+                HandleSensor(sensor, verboseOutput);
+            });
+                        
+            var cancellation = console.RegisterCancellationHandler();
+
+            var keepPooling = Pooling;
+            if (Verbose && Pooling)
+            {
+                Debug.Assert(verboseOutput != null);
+                await verboseOutput.WriteLineAsync("Press Ctrl+C to cancel");
             }
-            
+
+            do
+            {
+                verboseOutput?.WriteLine($"Reading sensors on {Environment.MachineName} at {DateTime.Now:yyyy-MM-dd HH:mm:ss}...");
+                computer.Reset();
+                computer.Accept(visitor);
+
+                if (!keepPooling)
+                {
+                    continue;
+                }
+
+                verboseOutput?.WriteLine($"Waiting {PoolingInterval}s for the next sensor reading. Ctrl+C to Cancel.");
+                var wait = TimeSpan.FromSeconds(PoolingInterval);
+                await Task.Delay(wait, cancellation);
+                if (cancellation.IsCancellationRequested)
+                {
+                    keepPooling = false;
+                }
+
+                if (keepPooling)
+                {
+                    verboseOutput?.WriteLine();
+                }
+               
+            } while (keepPooling);
+
             computer.Close();
         }
         catch (CommandException)
-        {
+        {            
             throw;
         }
-        catch(Exception ex)
+        catch (TaskCanceledException)
+        {
+            verboseOutput?.WriteLine("User cancelled!");
+            return;
+        }
+        catch (Exception ex)
         {
             throw new CommandException("Unknown exception!", 999, false, ex);
         }
 
-        return default;
+    }
+
+    private void HandleSensor(ISensor sensor, TextWriter? verbose)
+    {
+        if (!SensorTypes.Contains(sensor.SensorType))
+        {
+            return;
+        }
+
+        verbose?.WriteLine($"S {sensor.SensorType}: {sensor.Identifier}: {sensor.Name} = {sensor.Value}");
     }
 
     static bool IsUserAdministrator()
